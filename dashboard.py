eb194d63c52d596d4f3b94868f3d66955ec2ac8a
Ahmadpasa17 <160393527+Ahmadpasa17@users.noreply.github.com>
2024-03-06 11:45:36 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,7 +15,7 @@ def bymonth(df):
     return month_rent
 
 # import dataframe
-day_df = pd.read_csv(pwd + 'day_data.csv')
+day_df = pd.read_csv('day_data.csv')
 
 
 # Menyiapkan dataframe yang dikelompokkan
